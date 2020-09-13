72e3145e1cb35ef117f8fef2e1357c90cf681546
tanvirkabir47 <71209931+tanvirkabir47@users.noreply.github.com>
2020-09-14 00:35:23 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,103 @@
+jQuery(document).ready(function($){
+          $('.homepage-slides').owlCarousel({
+          	  items: 1,
+          	  nav:false,
+	          loop:true,
+	          autoplay:false,
+	          dots: true,
+	          margin:10,
+	          responsiveClass:true,
+	          responsive:{
+	              0:{
+	                  items:1,
+	                  nav:false,
+	                  dots:true,   
+	              },
+	              600:{
+	                  items:1,
+	                  nav:false,
+	                  dots:true,
+	              },
+	              1000:{
+	                  items:1,
+	                  nav:false,
+	                  loop:true,
+	                  dots:true,
+	              }
+              }
+      })
+
+  });
+
+
+jQuery(document).ready(function($){
+          $('.event-slider').owlCarousel({
+          	  items: 3,
+          	  nav:true,
+	          loop:true,
+	          autoplay:false,
+	          dots: false,
+	          margin:10,
+	          responsiveClass:true,
+	          responsive:{
+	              0:{
+	                  items:1,
+	                  nav:true,
+	                  dots:false,   
+	              },
+	              600:{
+	                  items:1,
+	                  nav:true,
+	                  dots:false,
+	              },
+	              1200:{
+	                  items:3,
+	                  nav:true,
+	                  loop:true,
+	                  dots:false,
+	              }
+              }
+      })
+
+  });
+
+
+
+
+jQuery(document).ready(function($){
+          $('.patner-slider-area').owlCarousel({
+          	  items: 3,
+          	  nav:true,
+	          loop:true,
+	          autoplay:false,
+	          dots: false,
+	          margin:10,
+	          responsiveClass:true,
+	          responsive:{
+	              0:{
+	                  items:3,
+	                  nav:true,
+	                  dots:false,   
+	              },
+	              600:{
+	                  items:3,
+	                  nav:true,
+	                  dots:false,
+	              },
+	              1200:{
+	                  items:6,
+	                  nav:true,
+	                  loop:true,
+	                  dots:false,
+	              }
+              }
+      })
+
+  });
+
+
+
+jQuery(document).ready(function($){
+	
+})
+
